75de1fba55560d406f44e6166c1eef546e559fab
Pouya Sadeghimirani <pooya@arianalabs.io>
2024-12-12 11:25:24 +0330

    feat: install tailwind plugins and update package file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,64 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+
+import { fontFamily } from "tailwindcss/defaultTheme";
+import tailwindFormPlugin from "@tailwindcss/forms";
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", '[class="dark-mode"]'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          900: withOpacity("--color-primary-900"),
+          800: withOpacity("--color-primary-800"),
+          700: withOpacity("--color-primary-700"),
+          600: withOpacity("--color-primary-600"),
+          500: withOpacity("--color-primary-500"),
+          400: withOpacity("--color-primary-400"),
+          300: withOpacity("--color-primary-300"),
+          200: withOpacity("--color-primary-200"),
+          100: withOpacity("--color-primary-100"),
+        },
+        secondary: {
+          900: withOpacity("--color-secondary-900"),
+          800: withOpacity("--color-secondary-800"),
+          700: withOpacity("--color-secondary-700"),
+          600: withOpacity("--color-secondary-600"),
+          500: withOpacity("--color-secondary-500"),
+          400: withOpacity("--color-secondary-400"),
+          300: withOpacity("--color-secondary-300"),
+          200: withOpacity("--color-secondary-200"),
+          100: withOpacity("--color-secondary-100"),
+          50: withOpacity("--color-secondary-50"),
+          0: withOpacity("--color-secondary-0"),
+        },
+        success: withOpacity("--color-success"),
+        warning: withOpacity("--color-warning"),
+        error: withOpacity("--color-error"),
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+      },
+      fontFamily: {
+        sans: ["var(--font-vazir)", ...fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    tailwindFormPlugin({
+      strategy: "class",
+    }),
+  ],
 };
